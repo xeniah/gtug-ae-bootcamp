53c18fe236d178ac1381e7ed665dd8acff78f335
Mike Koss <mckoss@startpad.org>
2012-01-06 15:13:23 -0800

    fixes for pf.py on python 2.5 - nees relpath

--- bin/pf.py
+++ bin/pf.py
@@ -31,7 +31,42 @@ try:
 except ImportError:
     import simplejson as json  # Please easy_install simplejson
 
-VERSION = '1.10.1'
+
+# Monkey-patch relpath into os.path if not implemented - Python 2.5
+if not hasattr(os.path, 'relpath'):
+    def relpath(path, start=os.curdir):
+        """Return a relative version of a path"""
+
+        if not path:
+            raise ValueError("no path specified")
+        start_list = os.path.abspath(start).split(os.sep)
+        path_list = os.path.abspath(path).split(os.sep)
+        if start_list[0].lower() != path_list[0].lower():
+            unc_path, rest = splitunc(path)
+            unc_start, rest = splitunc(start)
+            if bool(unc_path) ^ bool(unc_start):
+                raise ValueError("Cannot mix UNC and non-UNC paths (%s and %s)" %
+                                 (path, start))
+            else:
+                raise ValueError("path is on drive %s, start on drive %s" %
+                                 (path_list[0], start_list[0]))
+        # Work out how much of the filepath is shared by start and path.
+        for i in range(min(len(start_list), len(path_list))):
+            if start_list[i].lower() != path_list[i].lower():
+                break
+        else:
+            i += 1
+
+        rel_list = [os.pardir] * (len(start_list)-i) + path_list[i:]
+        if not rel_list:
+            return os.path.curdir
+        return os.path.join(*rel_list)
+
+    # Monkey-patch
+    os.path.relpath = relpath
+
+
+VERSION = '1.10.2'
 PF_DIST_SERVER = 'http://dist.pageforest.com'
 PF_DIST_DIRECTORY = '/directory.json'
 PF_FILENAME = 'pf.py'
